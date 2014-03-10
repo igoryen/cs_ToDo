218d4b3a1417dfcf02dfe8c4e5d4f78d30c6b4c3
igoryen <igor.yentaltsev@gmail.com>
2014-03-09 21:24:42 -0400

    Create an Entity Framework Code First ToDo model

---
+++ ToDo/Models/AppModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models {
+  public class AppModels {
+
+    //================================================================
+    // Since we are using Entity Framework Code First, EF will create 
+    // the right keys between Users and ToDo table.
+    //================================================================
+
+    //================================================================
+    // CREATE TABLE MyUser (HomeTown, ToDoes);
+    //================================================================
+    public class MyUser : IdentityUser {
+      public string HomeTown { get; set; }
+      public virtual ICollection<ToDo> ToDoes { get; set; }
+    }
+
+    //================================================================
+    // CREATE TABLE ToDo (Id, Description, IsDone, User);
+    //================================================================
+    public class ToDo {
+      public int Id { get; set; }
+      public string Description { get; set; }
+      public bool IsDone { get; set; }
+      public virtual MyUser User { get; set; }
+    }
+
+  }
+}
